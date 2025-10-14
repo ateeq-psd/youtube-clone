902f2252934cb52b79a68b0e6063c199892ece57
ateeq-psd <atiqueraza98@gmail.com>
2025-10-14 18:59:58 +0530

    Add sidebar

--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -2,6 +2,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar"
 import Link from "next/link"
 import Image from "next/image"
 import { SearchInput } from "./search-input"
+import { AuthButton } from "@/modules/auth/ui/components/auth-button"
 
 
 export const HomeNavbar = () => {
@@ -24,6 +25,11 @@ export const HomeNavbar = () => {
                 {/*Search Bar*/}
                 <div className="flex-1 flex justify-center max-w-[720px] mx-auto">
                     <SearchInput />
+                </div>
+                <div>
+                    <div className="flex-shrink-0 items-center flex gap-4">
+                        <AuthButton />
+                    </div>
                 </div>
             </div>
         </nav>
